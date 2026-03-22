c271abf30c9aab500c8bb952170e14e74c13628b
EC2 Default User <ec2-user@ip-172-31-54-156.ec2.internal>
2026-03-22 21:42:54 +0000

    Add whitenoise for static files

--- cpq_project/settings.py
+++ cpq_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fj=rw%d(y-$ow54dmt7r1jk0y-&mrlj2qk-q#*gv0zg=lv5smy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
